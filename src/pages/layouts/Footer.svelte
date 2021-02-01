9400aa4767f191fcddafd87964abc78780ac8fec
AriefRahmanW <rivurjr@gmail.com>
2021-02-01 11:55:15 +0700

    change _componets folder to layout

---
+++ src/pages/layouts/Footer.svelte
@@ -0,0 +1,15 @@
+<div class="w-full mt-8 py-4">
+    <p class="text-center font-bold text-lg">
+        Developed by Arief Rahman @2021
+    </p>
+</div>
+
+<style>
+    div{
+        background-color: #DFDECA;
+    }
+    p{
+        color: #3A3C42;
+    }
+    
+</style>
---
+++ src/pages/layouts/Search.svelte
@@ -0,0 +1,27 @@
+
+<script lang="ts">
+    import { filter } from "../../store";
+
+</script>
+
+<div class="flex items-center justify-center pt-5 px-20 md:px-40 lg:px-50 xl:px-60 text-center">
+    <div class="w-24 min-w-full">
+        <input
+            bind:value={$filter}
+            type="text" placeholder="" class="px-3 py-2 placeholder-gray-400 text-gray-700 bg-white bg-white rounded-full text-sm border border-gray-400 outline-none focus:outline-none focus:shadow-outline w-full"/>
+    </div>
+    <button
+        class="ml-2 px-8 md:px-10 py-2 placeholder-gray-400 text-white bg-white rounded-full text-sm outline-none focus:outline-none focus:shadow-outline">
+        Cari
+    </button>
+</div>
+
+<style>
+    input{
+        font-family: Raleway;
+    }
+    button{
+        font-family: Raleway;
+        background-color: #9CCD62;
+    }
+</style>
